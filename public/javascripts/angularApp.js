ec89bfe175bd4e3bcb194145c0969d0b8b87ab44
Thiago Assis <thiago@Thiagos-MacBook-Pro.local>
2016-05-21 22:41:02 -0300

    Persisting addPost

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -9,6 +9,12 @@ app.factory('posts', ['$http', function($http){
         angular.copy(data, o.posts);
       });
     };
+
+    o.create = function(post) {
+      return $http.post('/posts', post).success(function(data){
+        o.posts.push(data);
+      });
+    };
     return o;
 }]);
 
@@ -20,20 +26,10 @@ app.controller('MainCtrl', [
             if (!$scope.title || $scope.title === '') {
                 return;
             }
-            $scope.posts.push({
-                title: $scope.title,
-                link: $scope.link,
-                upvotes: 0,
-                comments: [{
-                    author: 'Joe',
-                    body: 'Cool post!',
-                    upvotes: 0
-                }, {
-                    author: 'Bob',
-                    body: 'Great idea but everything is wrong!',
-                    upvotes: 0
-                }]
-            });
+            posts.create({
+              title: $scope.title,
+              link: $scope.link
+            })
             $scope.title = '';
             $scope.link = '';
         };
