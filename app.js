7b57d390fdaac723656b8bca412b236e1fa71b15
Thiago Assis <thiagosil@gmail.com>
2016-05-23 18:03:03 -0300

    Partial registration process

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
 mongoose.connect('mongodb://localhost/news');
+
+require('./config/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -28,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
 var mongoose = require('mongoose');
+var jwt = require('express-jwt');
+
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
@@ -44,8 +49,9 @@ router.get('/posts', function(req, res, next) {
 });
 
 /* CREATE POSTS*/
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
+  post.author = req.payload.username;
 
   post.save(function(err, post){
     if(err){ return next(err); }
@@ -62,7 +68,7 @@ router.get('/posts/:post', function(req, res) {
 
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) { return next(err); }
 
@@ -70,9 +76,10 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author =  req.payload.username;
 
   comment.save(function(err, comment){
     if(err){ return next(err); }
@@ -86,7 +93,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next){
   req.comment.upvote(function(err, comment){
     if (err) { return next(err); }
 
@@ -94,7 +101,36 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
   })
 });
 
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
 
+  var user = new User();
+  user.username = req.body.username;
+  user.setPassword(req.body.password)
 
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+router.post('/login', function (req, res next) {
+  if(!req.body.username || !req.body.password){
+      return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local'. function(err, user, info){
+     if(err){ return next(err); }
+
+     if(user){
+       return res.json({token: user.generateJWT()});
+     } else {
+        return res.status(401).json(info);
+     }
+  })(req, res, next);
+});
 
 module.exports = router;
